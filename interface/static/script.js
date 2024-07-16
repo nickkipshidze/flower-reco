230ad152ef00e9263715b48a10fed410a2317a34
NickKipshidze <kipshidze.nick@gmail.com>
2024-07-16 06:33:21 +0400

    Preview uploaded image & handle file upload form

--- interface/static/script.js
+++ interface/static/script.js
@@ -10,8 +10,17 @@ document.getElementById("uploadForm").addEventListener("submit", function(event)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        if (data.image) {
+            const img = document.getElementById("uploadPreview");
+            img.src = `data:image/jpeg;base64,${data.image}`;
+        }
     })
     .catch(error => {
         console.error("Error:", error);
     });
+});
+
+document.getElementById("file").addEventListener("change", function() {
+    var fileName = this.files[0] ? this.files[0].name : "No file chosen";
+    document.getElementById("file-name").innerText = fileName;
 });
